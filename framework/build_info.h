e71c1f584e30fb57d6d2df3110a9e30f0253d087
jadis0x <49281043+jadis0x@users.noreply.github.com>
2025-10-28 13:02:48 +0300

    Update app version to a6.0.24

--- framework/build_info.h
+++ framework/build_info.h
@@ -3,7 +3,7 @@
 namespace BuildInfo
 {
     inline constexpr const char* kVersion = "v2.5";
-    inline constexpr const char* kAppVersion = "a6.0.23";
+    inline constexpr const char* kAppVersion = "a6.0.24";
     inline constexpr const char* kGitHubOwner = "Jadis0x";
     inline constexpr const char* kGitHubRepo = "DevourX";
 }
